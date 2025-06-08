f80d79c80b2d8cb4b5bb28e0012fa22de00dcb35
MarkGamboa <202310110@fit.edu.ph>
2025-06-08 18:42:41 +0800

    feat: Add header and footer components for main layout structure

---
+++ components/templates/footer.component.php
@@ -0,0 +1,10 @@
+</main>
+
+<footer class="footer" role="contentinfo">
+  <p>© 2024 SolarSoil. All rights reserved.</p>
+</footer>
+
+<script src="/assets/js/script.js"></script>
+</body>
+
+</html>
